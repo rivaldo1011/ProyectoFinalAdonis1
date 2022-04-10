bd4145e984f14e0d731db1ef6c3b7e84c0b241f0
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-10 12:31:44 -0500

    eliminar
    se elimino algo en rutas que solo era de prueba

--- hello-world/start/routes.ts
+++ hello-world/start/routes.ts
@@ -1,17 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
-
-Route.post('login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
-
-  try {
-    const token = await auth.use('api').attempt(email, password)
-    return token
-  } catch {
-    return response.badRequest('Invalid credentials')
-  }
-})
-
 Route.get('/', async () => {
   return { hello: 'world' }
 })
