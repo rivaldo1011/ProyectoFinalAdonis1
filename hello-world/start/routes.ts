4e87172c853933117d2be5c41b56af4f3f17585c
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-11 22:39:51 -0500

    edit
    se agrego todo dentro del grupo para que este protegido por el auth

--- hello-world/start/routes.ts
+++ hello-world/start/routes.ts
@@ -8,21 +8,35 @@ Route.group(() => {
     console.log(auth.use('api').user!)
     return { resp: 'activo' }
   })
+
+  //AUTH-SENSORES 
+  Route.get('smostrarSensores', 'SensorsController.getSensores')
+  Route.post('screarSensores', 'SensorsController.crearSensor')
+  Route.put('supdateSensores', 'SensorsController.updateSensores')
+  Route.delete('seliminarSensores', 'SensorsController.deleteSensor')
+
+  //AUTH-HISTORIAL
+  Route.get('smostrarHistorial', 'HistorialsController.getHistorial')
+  Route.post('screarHistorial', 'HistorialsController.Historial')
+  //AUTH-Historial
+  Route.get('sConexion', 'HistorialsController.mostrarHistorial')
+  Route.put('sInsertarHistorial', 'HistorialsController.store')
+  Route.get('sMostrarHistorial', 'HistorialsController.mostar')
 }).middleware('auth')
 //LOGIN
-  Route.post('Login','AuthController.Login')
-  Route.post('Register','AuthController.register')
+Route.post('Login', 'AuthController.Login')
+Route.post('Register', 'AuthController.register')
 //SENSORES 
-  Route.get('mostrarSensores','SensorsController.getSensores')
-  Route.post('crearSensores','SensorsController.crearSensor')
-  Route.put('updateSensores','SensorsController.updateSensores')
-  Route.delete('eliminarSensores','SensorsController.deleteSensor')
-  //HISTORIAL
-  Route.get('mostrarHistorial','HistorialsController.getHistorial')
-  Route.post('crearHistorial','HistorialsController.Historial')
-  //Historial
-  Route.get('Conexion','HistorialsController.mostrarHistorial')
-  Route.put('InsertarHistorial','HistorialsController.store')
-  Route.get('MostrarHistorial','HistorialsController.mostar')
-  //pruebas historial autoincrement
-  Route.get('autoincrement','HistorialsController.autoincrement')
+Route.get('mostrarSensores', 'SensorsController.getSensores')
+Route.post('crearSensores', 'SensorsController.crearSensor')
+Route.put('updateSensores', 'SensorsController.updateSensores')
+Route.delete('eliminarSensores', 'SensorsController.deleteSensor')
+//HISTORIAL
+Route.get('mostrarHistorial', 'HistorialsController.getHistorial')
+Route.post('crearHistorial', 'HistorialsController.Historial')
+//Historial
+Route.get('Conexion', 'HistorialsController.mostrarHistorial')
+Route.put('InsertarHistorial', 'HistorialsController.store')
+Route.get('MostrarHistorial', 'HistorialsController.mostar')
+//pruebas historial autoincrement
+Route.get('autoincrement', 'HistorialsController.autoincrement')
