aa4b993c1a61c8826dd534b0db57105f7289b7fb
echodegrasa <74998947+issacbrizuela1@users.noreply.github.com>
2022-04-20 01:16:04 -0500

    editar
    se agrego para que se insertara el registro en el historial según el sensor.

--- hello-world/app/Controllers/Http/HistorialsController.ts
+++ hello-world/app/Controllers/Http/HistorialsController.ts
@@ -3,21 +3,25 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Env from '@ioc:Adonis/Core/Env'
 import mongoose from 'mongoose'
 import schHistorial from 'App/Models/Historial'
+import DHT11M from 'App/Models/DHT11'
+import HC_SR04M from 'App/Models/HC_SR04'
+import MPU6050M from 'App/Models/MPU6050'
+import MotoresM from 'App/Models/Motores'
+import KY_031M from 'App/Models/KY_031'
 let URL = Env.get('MONGO_URL')
 let mongo = mongoose.connect(URL, { maxIdleTimeMS: 1000 });
 export default class HistorialsController {
-
   public async autoincrement() {
     try {
       const preb = (await mongo).model('historialsensores', schHistorial)
       let s = await preb.aggregate([{
         $project: {
-          id: 1,
+          idH: 1,
           _id: 0
         }
       }, {
         $sort: {
-          id: -1
+          idH: -1
         }
       }, { $limit: 1 }])
       let res
@@ -44,7 +48,8 @@ export default class HistorialsController {
     return buscar
   }
   //CREAR
-  public async crearHistorial({ request }: HttpContextContract) {
+  public async crearHistorial({ request, response }) {
+    /*
     const datos = request.all()
     const preb = (await mongo).model('historialsensores', schHistorial)
     let idd = await this.autoincrement()
@@ -64,14 +69,138 @@ export default class HistorialsController {
       .catch((err) => {
         console.log(err)
       })
+      */
+    const datos = request.all()
+    let ultimo = this.autoincrement()
+    let id = await ultimo + 1
+    switch (datos.idSensor) {
+      case 1:
+        await mongoose.connect(URL)
+        response = new DHT11M.DHT11M({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 2,
+          Temperatura: request.input('Temperatura'),
+          Humedad: request.input('Humedad'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+      case 2:
+        await mongoose.connect(URL)
+        response = new HC_SR04M.HC_SR04M({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 2,
+          Distacia: request.input('Distacia'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+      case 3:
+        await mongoose.connect(URL)
+        response = new KY_031M.KY_031M({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 3,
+          Deteccion: request.input('Deteccion'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+      case 4:
+        await mongoose.connect(URL)
+        response = new MPU6050M.MPU6050M({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 4,
+          acel_x: request.input('acel_x'),
+          acel_y: request.input('acel_y'),
+          acel_z: request.input('acel_z'),
+          ang_x: request.input('ang_x'),
+          ang_y: request.input('ang_y'),
+          ang_z: request.input('ang_z'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+      case 5:
+        await mongoose.connect(URL)
+        response = new MotoresM.MotoresM({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 5,
+          Posicion: request.input('Posicion'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+      case 6:
+        await mongoose.connect(URL)
+        response = new MotoresM.MotoresM({
+          idH: id,
+          idRU: request.input('idRU'),
+          idSensor: 6,
+          Posicion: request.input('Posicion'),
+          Fechacreacion: Date.now()
+        }).then((data) => {
+          console.log(data)
+          return data
+        })
+          .catch((err) => {
+            console.log(err)
+          })
+        response.save()
+        return response
+        break;
+
+      default:
+        return response.badRequest()
+        break;
+    }
   }
   //editar
-  public async updateHistorial({ params,request,response }: HttpContextContract) {
+  public async updateHistorial({ params, request, response }: HttpContextContract) {
     const datos = request.all()
     //let date = new Date()
     //let [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()]
     const preb = (await mongo).model('historialsensores', schHistorial)
-    
+
     preb
       .updateOne({ id: params.id }, {
         idSensor: datos.idSensor,
@@ -86,11 +215,11 @@ export default class HistorialsController {
       })
   }
   //eliminar
-  public async deleteSensor({params, response }: HttpContextContract) {
-    
+  public async deleteSensor({ params, response }: HttpContextContract) {
+
     const preb = (await mongo).model('historialsensores', schHistorial)
     preb
-      .deleteOne({id: params.id })
+      .deleteOne({ id: params.id })
       .then((data) => {
         return response.finished
         //console.log(data)
